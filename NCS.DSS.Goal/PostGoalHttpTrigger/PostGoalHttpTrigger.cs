cd78ca5b748333b9401e65a35c1582d21d46edc9
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:49:51 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Goal/PostGoalHttpTrigger/PostGoalHttpTrigger.cs
+++ NCS.DSS.Goal/PostGoalHttpTrigger/PostGoalHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Goal.PostGoalHttpTrigger
     public static class PostGoalHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Goals/")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Goal))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Goals/")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Goal C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var goal = await req.Content.ReadAsAsync<Models.Goal>();
+
+            var goalService = new PostGoalHttpTriggerService();
+            var goalId = goalService.Create(goal);
+
+            return goalId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Goal record with Id of : " + goalId)
+                };
         }
     }
 }
--- NCS.DSS.Goal/PostGoalHttpTrigger/PostGoalHttpTriggerService.cs
+++ NCS.DSS.Goal/PostGoalHttpTrigger/PostGoalHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Goal.PostGoalHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Goal.PostGoalHttpTrigger
 {
     public class PostGoalHttpTriggerService
     {
+        public Guid? Create(Models.Goal goal)
+        {
+            if (goal == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
