7031a69dc5f14fa7434043eef9245363c8caeebc
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-18 14:29:21 +0100

    Add Validation

---
+++ NCS.DSS.Goal/Validation/IValidate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NCS.DSS.Goal.Validation
+{
+    public interface IValidate
+    {
+        List<ValidationResult> ValidateResource<T>(T resource);
+    }
+}
